f915719aa5888c4a74e081845a99c59c812001bc
Geraldi-Mejia <geraldi.mejia@ucr.ac.cr>
2022-10-27 16:56:11 -0600

    Home And Principal Website

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import logo from './img/OracleA.png'
+export default function Home(){
+    return (<div>
+        <h1>Bienvenido al Proyecto de Redes en los negocios</h1>
+        <div className='home-content'>
+        <p>En este proyecto se ponen en practica los conceptos vistos en clase de ciber-seguridad,
+            Cloud computing mediante la plataforma de Oracle y autenticación además Integración continua y despliegue continuo.
+        </p>
+        <img className='home-image' src={logo}/>
+        </div>
+    </div>)
+}
