aa0752c012d8dca0cfefff7263acf4f811a26146
Geraldi-Mejia <geraldi.mejia@ucr.ac.cr>
2022-10-27 17:01:23 -0600

    Alt prop added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export default function Home(){
         <p>En este proyecto se ponen en practica los conceptos vistos en clase de ciber-seguridad,
             Cloud computing mediante la plataforma de Oracle y autenticación además Integración continua y despliegue continuo.
         </p>
-        <img className='home-image' src={logo}/>
+        <img className='home-image' src={logo} alt= "Logo de Oracle"/>
         </div>
     </div>)
 }
