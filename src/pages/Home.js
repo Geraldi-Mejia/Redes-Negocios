dad42a53b9340069799a7094f535db710f2ad821
Geraldi-Mejia <geraldi.mejia@ucr.ac.cr>
2022-10-27 18:14:22 -0600

    Testing modify

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import logo from './img/OracleA.png'
 export default function Home(){
     return (<div>
-        <h1>Bienvenido al Proyecto de Redes en los negocios</h1>
+        <h1>Bienvenido al Proyecto de Redes en los negocios 2</h1>
         <div className='home-content'>
         <p>En este proyecto se ponen en practica los conceptos vistos en clase de ciber-seguridad,
             Cloud computing mediante la plataforma de Oracle y autenticación además Integración continua y despliegue continuo.
