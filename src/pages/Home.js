a376c19109a4e1a0e21cbb2205b0b68272d3eba4
Geraldi-Mejia <geraldi.mejia@ucr.ac.cr>
2022-11-04 11:20:16 -0600

    Link images to bucket

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
-import logo from './img/OracleA.png'
-export default function Home(){
+export default function Home() {
     return (<div>
         <h1>Bienvenido al Proyecto de Redes en los negocios</h1>
         <div className='home-content'>
-        <p>En este proyecto se ponen en practica los conceptos vistos en clase de ciber-seguridad,
-            Cloud computing mediante la plataforma de Oracle y autenticación además Integración continua y despliegue continuo.
-        </p>
-        <img className='home-image' src={logo} alt= "Logo de Oracle"/>
+            <p>En este proyecto se ponen en practica los conceptos vistos en clase de ciber-seguridad,
+                Cloud computing mediante la plataforma de Oracle y autenticación además Integración continua y despliegue continuo.
+            </p>
+            <div>
+                <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6okqSjFUDztccok-RtYNXFfX4yf2Y--pvXG1kuML0WNrS3483LSwL2Gtx0fapnIe/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleA.png'
+                    alt="Logo de Oracle A" />
+
+
+              {/*   <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6p0Y94siFwlZVj6vb_D2ou7oIHtcglbKIlYIqLZVnqtlE5mtRvRdc-sXFdt1QIDh/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleB.png'
+                    alt="Logo de Oracle B" />
+                    */}
+            </div>
         </div>
-        
+
     </div>)
 }
