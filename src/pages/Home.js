7fe9113e239624f39c1a1ea4f8f7a67a57cb8989
Geraldi-Mejia <geraldi.mejia@ucr.ac.cr>
2022-11-17 15:56:01 -0600

    FInal Test DI/CD

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,12 @@ export default function Home() {
                 Cloud computing mediante la plataforma de Oracle y autenticación además Integración continua y despliegue continuo.
             </p>
             <div>
-                {/*<img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6okqSjFUDztccok-RtYNXFfX4yf2Y--pvXG1kuML0WNrS3483LSwL2Gtx0fapnIe/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleA.png'
+                <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6okqSjFUDztccok-RtYNXFfX4yf2Y--pvXG1kuML0WNrS3483LSwL2Gtx0fapnIe/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleA.png'
                     alt="Logo de Oracle A" />
-*/}
 
-                 <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6p0Y94siFwlZVj6vb_D2ou7oIHtcglbKIlYIqLZVnqtlE5mtRvRdc-sXFdt1QIDh/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleB.png'
-                    alt="Logo de Oracle B" />
+
+                {/* <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6p0Y94siFwlZVj6vb_D2ou7oIHtcglbKIlYIqLZVnqtlE5mtRvRdc-sXFdt1QIDh/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleB.png'
+                    alt="Logo de Oracle B" />*/}
                     
             </div>
             <p>Geraldi Mejia Segura.</p>
