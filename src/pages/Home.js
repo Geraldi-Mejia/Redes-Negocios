b6715c0cacf56a85ac5bec3142ee548d449aae53
Geraldi-Mejia <geraldi.mejia@ucr.ac.cr>
2022-11-17 19:55:30 -0600

    Revision2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
 export default function Home() {
     return (<div>
-        <h1>Bienvenido al Proyecto de Redes En Los Negocios</h1>
+        <h1>Bienvenido al Proyecto de Redes Revision2  </h1>
         <div className='home-content'>
             <p>En este proyecto se ponen en practica los conceptos vistos en clase de ciber-seguridad,
                 Cloud computing mediante la plataforma de Oracle y autenticación además Integración continua y despliegue continuo.
             </p>
             <div>
-                <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6okqSjFUDztccok-RtYNXFfX4yf2Y--pvXG1kuML0WNrS3483LSwL2Gtx0fapnIe/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleA.png'
+                {/* <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6okqSjFUDztccok-RtYNXFfX4yf2Y--pvXG1kuML0WNrS3483LSwL2Gtx0fapnIe/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleA.png'
                     alt="Logo de Oracle A" />
+                */}
 
-
-                {/* <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6p0Y94siFwlZVj6vb_D2ou7oIHtcglbKIlYIqLZVnqtlE5mtRvRdc-sXFdt1QIDh/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleB.png'
-                    alt="Logo de Oracle B" />*/}
+                <img className='home-image' src='https://objectstorage.us-ashburn-1.oraclecloud.com/p/6p0Y94siFwlZVj6vb_D2ou7oIHtcglbKIlYIqLZVnqtlE5mtRvRdc-sXFdt1QIDh/n/id2e3kgx0uik/b/Proyecto-Redes/o/OracleB.png'
+                    alt="Logo de Oracle B" />
                     
             </div>
             <p>Geraldi Mejia Segura.</p>
